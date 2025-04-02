111a4ec15dde6458ac0ff838a104fc17230d0002
pk <pavel_kohout@centrum.cz>
2025-04-02 13:30:32 +0200

    add cors

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
+app.use(cors());
 
 let users = [
   { id: 1, name: "John Doe", email: "john@example.com" },
