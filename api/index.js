6ca39e55fe85191478abbe1adf1f25f9a631f7c1
pk <pk@pk.com>
2025-04-14 10:02:52 +0200

    update cors url

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const allowedOrigins = [
-  "https://localhost:5173",
+  "http://localhost:5173",
   "https://test-api-ui-teal.vercel.app/",
 ];
 
