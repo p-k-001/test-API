0d07bf5b731ef88bd178cae1196345f2bee77858
pk <pk@pk.com>
2025-04-14 10:46:42 +0200

    update env

--- api/index.js
+++ api/index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://test-api-ui-teal.vercel.app/",
+  "https://test-api-ui-teal.vercel.app",
 ];
 
 app.use(bodyParser.json());
