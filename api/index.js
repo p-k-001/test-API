75d8ef909b032c5ef66d169fe2134a9188e764a3
pk <pavel_kohout@centrum.cz>
2025-04-16 10:59:15 +0200

    clear db

--- api/index.js
+++ api/index.js
@@ -33,8 +33,8 @@ app.get("/api-docs/swagger.json", (req, res) => {
 });
 
 let users = [
-  { id: 1, name: "John Doe", email: "john@example.com" },
-  { id: 2, name: "Jane Smith", email: "jane@example.com" },
+  // { id: 1, name: "John Doe", email: "john@example.com" },
+  // { id: 2, name: "Jane Smith", email: "jane@example.com" },
 ];
 
 /**
